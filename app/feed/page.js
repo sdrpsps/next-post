77fa9feb024205ab352f93a71fd6e64bf413a29b
sunny <sunny@bytespark.app>
2024-09-20 15:49:46 +0800

    metadata optimization

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,13 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export const generateMetadata = async () => {
+  const posts = await getPosts();
+  return {
+    title: `Browse all ${posts.length} posts`,
+    description: `Browse all our posts from the community`,
+  };
+};
 
 export default async function FeedPage() {
   const posts = await getPosts();
